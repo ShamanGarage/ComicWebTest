aeb7a2931c1ce2a3a3d45ad9faeccf1da893f608
Pabgre <pabgre@pop-os.localdomain>
2020-05-14 14:33:31 +0200

    retocado inicio adv3

--- scripts/adventur3.js
+++ scripts/adventur3.js
@@ -17,7 +17,8 @@ function adventure3()
         }
 
     });
-    getElem("3_04").style.opacity = "100%"
+    getElem("3_02").style.opacity = "100%"
+    triggers["3_tr03"].set_active(true)
 
     constelaciones = [false, false, false, false, false]
 
@@ -25,48 +26,57 @@ function adventure3()
 
     cell_img = ["3_05_6","3_06", "3_06_1", "3_06_2", "3_06_3", "3_06_4", "3_06_5", "3_06_6"]
 
-    for (let index = 1; index < 6; index++) {
-        let i = index
-        let trId = "3_tr04_" + i
-        triggers[trId].set_active(true)
-        //Redefinimos la funcion de hacer clic a cada constelacion
-        triggers[trId].trObj.onclick = function(){
-            if (triggers[trId].active)
-            {
-                //Hacemos que se notifique el clic
-                constelaciones[i - 1] = true
-                triggers[trId].action()
-                //Si se han clicado todas las constelaciones, mostramos
-                //la celula y activamos los triggers de cada componente
-                if(all_true(constelaciones))
-                {
-                    getElem("3_05").style.opacity = "100%"
-                    for (let j = 1; j < 6; j++) {
-                        let j2 = j
-                        let subTrId = "3_tr05_" + j2
-                        triggers[subTrId].set_active(true)
-                        //Redefinimos la funcion de hacer clic a cada componente
-                        triggers[subTrId].trObj.onclick = function(){
-                            componentes[j2 - 1] = true
-                            triggers[subTrId].action()
-                            //Si ya estan todas las componentes en la celula
-                            //las cambiamos por la imagen total y activamos el trigger
-                            //para compactar todos los elementos
-                            if (all_true(componentes))
-                            {
-                                for (let k = 1; k < 6; k++) {
-                                    let subId = "3_05_" + k
-                                    getElem(subId).style.opacity = "0%"
+    triggers["3_tr03"].add_new_trigger(triggers["3_tr04"])
+
+    triggers["3_tr04"].trObj.onclick = function()
+    {
+        if (triggers["3_tr04"].active)
+        {
+            triggers["3_tr04"].action()
+            for (let index = 1; index < 6; index++) {
+                let i = index
+                let trId = "3_tr04_" + i
+                triggers[trId].set_active(true)
+                //Redefinimos la funcion de hacer clic a cada constelacion
+                triggers[trId].trObj.onclick = function(){
+                    if (triggers[trId].active)
+                    {
+                        //Hacemos que se notifique el clic
+                        constelaciones[i - 1] = true
+                        triggers[trId].action()
+                        //Si se han clicado todas las constelaciones, mostramos
+                        //la celula y activamos los triggers de cada componente
+                        if(all_true(constelaciones))
+                        {
+                            getElem("3_05").style.opacity = "100%"
+                            for (let j = 1; j < 6; j++) {
+                                let j2 = j
+                                let subTrId = "3_tr05_" + j2
+                                triggers[subTrId].set_active(true)
+                                //Redefinimos la funcion de hacer clic a cada componente
+                                triggers[subTrId].trObj.onclick = function(){
+                                    componentes[j2 - 1] = true
+                                    triggers[subTrId].action()
+                                    //Si ya estan todas las componentes en la celula
+                                    //las cambiamos por la imagen total y activamos el trigger
+                                    //para compactar todos los elementos
+                                    if (all_true(componentes))
+                                    {
+                                        for (let k = 1; k < 6; k++) {
+                                            let subId = "3_05_" + k
+                                            getElem(subId).style.opacity = "0%"
+                                        }
+                                        getElem("3_05_6").style.opacity = "100%"
+                                        set_image_sequence_tr(triggers["3_tr06"], triggers["3_tr07"], cell_img, 8)
+                                        triggers["3_tr06"].set_active(true)
+                                    }
                                 }
-                                getElem("3_05_6").style.opacity = "100%"
-                                set_image_sequence_tr(triggers["3_tr06"], triggers["3_tr07"], cell_img, 8)
-                                triggers["3_tr06"].set_active(true)
                             }
                         }
                     }
+
                 }
             }
-
         }
     }
 
